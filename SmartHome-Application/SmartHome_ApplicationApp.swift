d1f651c55f68a9427b84f4e2b6b703d5e2f2b5ad
dionamuciqi <diona.muqiqi3@gmail.com>
2024-02-27 22:27:00 +0100

    Initial Commit

---
+++ SmartHome-Application/SmartHome_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHome_ApplicationApp.swift
+//  SmartHome-Application
+//
+//  Created by Diona Muciqi on 27.2.24.
+//
+
+import SwiftUI
+
+@main
+struct SmartHome_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
