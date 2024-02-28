e5059fc6cd3654ed48a0405799f18b717c8d61e1
Dominik <130363380+dominikpllashniku@users.noreply.github.com>
2024-02-28 17:37:49 +0100

    Update RectangleModel.swift
    some changes  on this model

--- SmartHome/Model/RectangleModel.swift
+++ SmartHome/Model/RectangleModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Climate: Identifiable {
+  var id = UUID()
+  let color: String
+  let image: String
+  let type: String
+  let device: String
+}import Foundation
+import SwiftUI
+
+struct Climate: Identifiable {
+  var id = UUID()
+  let color: String
+  let image: String
+  let type: String
+  let device: String
+}
