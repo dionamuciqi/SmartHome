15e591addd8ebb32433619f28557b68090ae9e5e
Dominik <130363380+dominikpllashniku@users.noreply.github.com>
2024-02-28 17:38:55 +0100

    Update Light.swift
    Some changes

--- SmartHome/Model/Light.swift
+++ SmartHome/Model/Light.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Light: Identifiable {
+  var id = UUID()
+  let image: String
+  let name: String
+  let sub: String
+}
+struct N: Identifiable {
+    var id: Int
+    var active: Bool = false
+
+}
