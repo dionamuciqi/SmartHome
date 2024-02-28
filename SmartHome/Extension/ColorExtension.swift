b81c6223e1a8ea0095711b2363e6bf7d51e3fdad
Dominik <130363380+dominikpllashniku@users.noreply.github.com>
2024-02-28 17:40:09 +0100

    Update ColorExtension.swift
    Changes in color

--- SmartHome/Extension/ColorExtension.swift
+++ SmartHome/Extension/ColorExtension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import SwiftUI
+extension Color {
+
+  static let theme = Colors()
+}
+
+struct Colors {
+  let blue = Color("Blue")
+  let circle = Color("Circle")
+  let radial = Color("Radial")
+  let purpleColor = Color("purple")
+}
