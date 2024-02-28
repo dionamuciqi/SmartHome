155898693e8819323bcf9a103208d008271297db
dominik <dominik.pllashniku@student.uni-pr.edu>
2024-02-28 14:31:35 +0100

    second view done

--- SmartHome/Core/SecondView/Grid.swift
+++ SmartHome/Core/SecondView/Grid.swift
@@ -5,4 +5,27 @@
 //  Created by adriani on 28.02.24.
 //
 
-import Foundation
+import SwiftUI
+
+struct Grid: View {
+    var body: some View {
+
+        ZStack {
+          
+          RoundedRectangle(cornerRadius: 35)
+            .fill(Color(red: 0.07, green: 0.08, blue: 0.12))
+            .frame(width: 360, height: 450)
+
+          GridElement()
+
+        }
+      
+    }
+}
+
+struct Grid_Previews: PreviewProvider {
+    static var previews: some View {
+        Grid()
+    }
+}
+
